054377b943bc0c232959bda530e98c181c8f8524
zeroho931 <77812192+zeroho931@users.noreply.github.com>
2024-03-07 16:18:55 +0900

    워들 게임 완성
    워들게임완성

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+
+answer = 'TRAIN'
+
+@app.get('/answer')
+def get_answer():
+    return {'answer':answer}
+
+app.mount("/", StaticFiles(directory="static",html=True), name="static")
